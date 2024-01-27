f3d8a1cea1170941dcbbc5c9dbfe8a90c47d260f
fengjinlong <18618166564@163.com>
2024-01-27 11:47:45 +0800

    1

--- vite-run/App.jsx
+++ vite-run/App.jsx
@@ -5,14 +5,24 @@ function Foo() {
   const [bar, setBar] = React.useState(20);
   function handleClick() {
     setCount((c) => c + 1);
+  }
+  function handleClickBar() {
     setBar((c) => c + 10);
   }
+  React.useEffect(() => {
+    console.log("init effect");
+  }, []);
+
+  React.useEffect(() => {
+    console.log(" count & bar init & change effect");
+  }, [count, bar]);
 
   return (
     <div>
       {count}
       <p>{bar}</p>
       <button onClick={handleClick}>foo click </button>
+      <button onClick={handleClickBar}>bar click </button>
     </div>
   );
 }
